eae0b190465567e890cc2930eceb68992085dcf3
Travis Lindsey <reachjtl@gmail.com>
2021-08-12 20:01:13 -0400

    Updated comments

--- generateDeployKey.sh
+++ generateDeployKey.sh
@@ -1,19 +1,7 @@
 #!/bin/sh
-# This script generates a ssh key for a single repository
-# and adds a custom configuration to the users (not global) ssh config file,
-# and outputs the public key for you to copy and paste as the repo deploy key
-# and outputs the url for you to clone the repo on the machine.
-# Github docs ref:
-# https://docs.github.com/en/developers/overview/managing-deploy-keys#using-multiple-repositories-on-one-server
-#
-# 1. Add the script to the user account of the machine. The home directory is fine.
-# 2. Make the script executable by running the following command as the user:
-# chmod u+x generateDeployKey.sh
-# 3. Run script like `./generateDeployKey.sh REPO_OWNER_NAME REPO_NAME` Note the space between owner and repo name. Example:
-# ./generateDeployKey.sh yourname hello_world
-# If you make a mistake with what you pass in, you can remove change from your ~/.ssh/config file
-# by deleting the most recent "New Key Generated on...." and deleting the related .pub and private keys
-
+# This script generates a ssh deploy key on a server for use with a single github repository
+# Docs at https://github.com/jtlindsey/github-generateDeployKey
+# Run with ./generateDeployKey.sh REPO_OWNER_NAME REPO_NAME
 
 # Check if user passed in both parameters
 if [ -z "$1" ] || [ -z "$2" ]
